da0d37d76c25ebd4cf25db16aeff94f416fc9ab0
EvilPuppet <evilpuppet@gmail.coml>
2019-11-25 22:28:11 -0300

    www

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,89 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// import { Container } from './styles';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-export default function Cart() {
-  return <div />;
+import api from '../../service/api';
+
+import { Container, ProductTable, Total } from './styles';
+
+export default class Cart extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cart: [],
+      total: '',
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const response = await api.get(`/products`);
+    this.setState({});
+  }
+
+  render() {
+    const { cart, total } = this.state;
+
+    return (
+      <Container>
+        <ProductTable>
+          <tread>
+            <tr>
+              <th />
+              <th>Produto</th>
+              <th>QTD</th>
+              <th>SUBTOTAL</th>
+              <th />
+            </tr>
+          </tread>
+          <tbody>
+            {cart.map(product => (
+              <tr>
+                <td>
+                  <img src={product.image} alt={product.title} />
+                </td>
+                <td>
+                  <strong>{product.title}</strong>
+                  <span>{product.priceFormatted}</span>
+                </td>
+                <td>
+                  <div>
+                    <button type="button">
+                      <MdRemoveCircleOutline size={20} color="#7159c1" />
+                    </button>
+                    <input type="number" readOnly />
+                    <button type="button">
+                      <MdAddCircleOutline size={20} color="#7159c1" />
+                    </button>
+                  </div>
+                </td>
+                <td>
+                  <strong>{product.subtotal}</strong>
+                </td>
+                <td>
+                  <button type="button">
+                    <MdDelete size={20} color="#7159c1" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </ProductTable>
+        <footer>
+          <button type="button">Finalizar pedido</button>
+
+          <Total>
+            <span>TOTAL</span>
+            <strong>{total}</strong>
+          </Total>
+        </footer>
+      </Container>
+    );
+  }
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import api from '../../service/api';
 
-export default function Home() {
-  return <div />;
+import { ProductList } from './styles';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      amount: '',
+      loading: false,
+    };
+  }
+
+  async componentDidMount() {
+    const { products } = this.state;
+    const response = await api.get('/products');
+
+    this.setState({
+      products: [...products, response.data],
+      amount: '',
+      loading: true,
+    });
+  }
+
+  render() {
+    const { products, amount, loading } = this.state;
+
+    if (loading) {
+      return <h1>Loading</h1>;
+    }
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <strong>{product.title}</strong>
+            <span>{product.price}</span>
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />{' '}
+                {amount[product.id] || 0}
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
